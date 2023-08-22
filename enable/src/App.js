c1399c3e0fd037e10f58b7cd578b7bf828cba97a
LekshAS12 <lekshmiajith04@gmail.com>
2023-08-22 17:38:00 +0530

    Incident form changes

--- enable/src/App.js
+++ enable/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css'; // You can create this CSS file for styling
-import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthComponent } from './authComponent/AuthComponent';
-
+//import { BrowserRouter as Router } from 'react-router-dom';
+//import { AuthComponent } from './authComponent/AuthComponent';
+import IncidentForm from './IncidentForm';
 function App() {
   return (
-   <Router>
+   /*<Router>
     <AuthComponent/>
-   </Router>
+  </Router>*/
+  <IncidentForm/>
   );
 }
 
